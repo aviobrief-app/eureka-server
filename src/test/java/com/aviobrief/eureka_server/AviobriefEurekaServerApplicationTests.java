e18a846134d16bd28aeed4492a00c057ddec63a8
Petar-I-Petkov <cpt.petkov@gmail.com>
2021-05-09 06:48:12 +0300

    Init commit: src & pom.xml

---
+++ src/test/java/com/aviobrief/eureka_server/AviobriefEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviobrief.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviobriefEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
